557d56d2919663ad82d0f99f3ee05bb757d73d83
Riche0826 <40441120@gm.nfu.edu.tw>
2019-06-22 00:05:04 +0800

    Initial commit

---
+++ app/src/main/java/flag/com/tw/ch04_massager/MainActivity.java
@@ -0,0 +1,33 @@
+package flag.com.tw.ch04_massager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Vibrator;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView txv = (TextView) findViewById(R.id.txv);
+        txv.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                Vibrator vb = (Vibrator) getSystemService(
+                        Context.VIBRATOR_SERVICE);
+
+                if(motionEvent.getAction() == MotionEvent.ACTION_DOWN) vb.vibrate(5000);
+                else if(motionEvent.getAction() == MotionEvent.ACTION_UP) vb.cancel();
+
+                return false;
+            }
+        });
+    }
+}
